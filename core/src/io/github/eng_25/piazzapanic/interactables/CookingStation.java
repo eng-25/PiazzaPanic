91bb2e093227d2d332dfa6dd29588c690903c1d0
Samuel Nicholson <sn1155@york.ac.uk>
2023-01-27 12:57:39 +0000

    Start of base of Interaction based classes (2)

--- core/src/io/github/eng_25/piazzapanic/interactables/CookingStation.java
+++ core/src/io/github/eng_25/piazzapanic/interactables/CookingStation.java
@@ -1,4 +1,16 @@
 package io.github.eng_25.piazzapanic.interactables;
 
+import io.github.eng_25.piazzapanic.common.ingredient.Ingredient;
+
+import java.util.List;
+
 public class CookingStation extends InteractionStation {
+    // Has a list of ingredients that it checks if the input is one of them
+    // (or is a list being inputed?)
+
+    public List<Ingredient> validInputs;
+    public Ingredient output;
+
+    // what are the methods for this?
+    //
 }
